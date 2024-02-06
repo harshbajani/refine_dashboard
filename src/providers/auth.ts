d95409a59f5d1a3d4216318734363cedefd22b9d
unknown <harshbajani7@gmail.com>
2024-02-06 18:18:54 +0530

    first commit

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -4,7 +4,7 @@ import { API_URL, dataProvider } from "./data";
 
 // For demo purposes and to make it easier to test the app, you can use the following credentials
 export const authCredentials = {
-  email: "michael.scott@dundermifflin.com",
+  email: "jim.halpert@dundermifflin.com",
   password: "demodemo",
 };
 
